d73ae54ce4045ae2f745308ca26938fccbb5cde2
Ali Arda İsenkul <ardaisenkul@hotmail.com>
2019-11-30 19:30:13 +0300

    Form App

---
+++ FormApp1/FormApp1/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormApp1.Controllers
+{
+    
+    public class ContactController : Controller
+    {
+        public IActionResult Send([FromBody]FormApp1.Data.ContactSendDto contactSendDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Hayırdır?");
+            }
+            return new JsonResult("ok");
+        }
+    }
+}
