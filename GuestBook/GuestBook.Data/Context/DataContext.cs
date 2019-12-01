c0caf407ce18ee84e3b90a4dda5ea45c294a54a4
Ali Arda İsenkul <ardaisenkul@hotmail.com>
2019-12-01 19:38:39 +0300

    GuestBook App

---
+++ GuestBook/GuestBook.Data/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using GuestBook.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuestBook.Data.Context
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+
+        public DbSet<GuestNote> GuestNotes { get; set; }
+    }
+
+}
---
+++ GuestBook/GuestBook/Controllers/GuestBookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GuestBook.Data.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SendAction([FromBody]GuestBookSendActionDto guestBookSendActionDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Bizde yapardık zamanında");
+            }
+            
+            return new JsonResult("ok");
+        }
+    }
+}
