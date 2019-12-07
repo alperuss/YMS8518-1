ab0672ac248efbb5373dcf8329d5cf4a42f1da51
Ali Arda İsenkul <ardaisenkul@hotmail.com>
2019-12-07 13:03:23 +0300

    yeni ödev eklendi

---
+++ MVCHomework/HomeworkMVC.Data/Context/DataContext.cs
@@ -0,0 +1,18 @@
+﻿
+using HomeworkMVC.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeworkMVC.Data.Context
+{
+    public class DataContext: DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+    }
+}
---
+++ MVCHomework/HomeworkMVC.Data/Model/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace HomeworkMVC.Data.Model
+{
+    public class Employee
+    {
+
+    public int Id { get; set; }
+        [Required, MinLength(2), MaxLength(50)]
+        public string Name { get; set; }
+        [Required, MinLength(2), MaxLength(50)]
+        public string SurName { get; set; }
+        [Required,Range(18,70)]
+    public int Age { get; set; }
+    }
+}
+
