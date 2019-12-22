e1cc8ec9bb87a8b88afcd938ff6afbc5037a8112
Ali Arda İsenkul <ardaisenkul@hotmail.com>
2019-12-22 18:08:33 +0300

    Repository Design Patterns(22.12.2019)

---
+++ WebApi2/WebApi2/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly Interfaces.IUserRepository _userRepository;
+
+        public UsersController(Interfaces.IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Models.User> users = _userRepository.List();
+            return new JsonResult(users);
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            Models.User user = _userRepository.Get(id);
+            return new JsonResult(user);
+        }
+        [HttpPost]
+        public IActionResult Insert([FromBody] Models.User user)
+        {
+            _userRepository.Insert(user);
+            return new JsonResult(user);
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Models.User user)
+        {
+            _userRepository.Update(user);
+            return new JsonResult(user);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userRepository.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ WebApi2/WebApi2/Services/UserRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi2.Models;
+
+namespace WebApi2.Services
+{
+    public class UserRepository : Interfaces.IUserRepository
+    {
+        private readonly DataContext _dataContext;
+        public UserRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public void Delete(int id)
+        {
+            Models.User user = Get(id);
+            _dataContext.Users.Remove(user);
+            _dataContext.SaveChanges();
+        }
+
+        public User Get(int id)
+        {
+            return _dataContext.Users.Find(id);
+        }
+
+        public User Insert(User user)
+        {
+            _dataContext.Users.Add(user);
+            _dataContext.SaveChanges();
+            return user;
+        }
+
+        public List<User> List()
+        {
+            return _dataContext.Users.ToList();
+        }
+
+        public User Update(User user)
+        {
+            _dataContext.Users.Update(user);
+            _dataContext.SaveChanges();
+            return user;
+        }
+    }
+}
