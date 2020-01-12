d1d36bb5d6fd0d1a2a34221b66765cc9962f8e41
Ali Arda İsenkul <34447040+aardaisenkul@users.noreply.github.com>
2020-01-12 16:31:05 +0300

    Add files via upload

--- SampiyonTakim/SampiyonTakim/Models/Category.cs
+++ SampiyonTakim/SampiyonTakim/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace SampiyonTakim.Models
 {
     public class Category
     {
+        public int Id { get; set; }
+        [Required, MaxLength(15)]
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public Byte[] Image { get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
     }
 }
--- SampiyonTakim/SampiyonTakim/Models/CustomerCustomerDemo.cs
+++ SampiyonTakim/SampiyonTakim/Models/CustomerCustomerDemo.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SampiyonTakim.Models
 {
+    
     public class CustomerCustomerDemo
     {
+      
+        [Required, MaxLength(5)]
+        public string CustomerId { get; set; }
+
+        [Required, MaxLength(10)]
+        public string Id { get; set; }
+
+
+
     }
 }
