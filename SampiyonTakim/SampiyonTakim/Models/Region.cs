7734a58220eadc2efff66b7e37ab54e65148b773
Ali Arda İsenkul <34447040+aardaisenkul@users.noreply.github.com>
2020-01-12 16:32:28 +0300

    Add files via upload

--- SampiyonTakim/SampiyonTakim/Models/Region.cs
+++ SampiyonTakim/SampiyonTakim/Models/Region.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace SampiyonTakim.Models
 {
     public class Region
     {
+        public int Id { get; set; }
+        [Required, MaxLength(50)]
+        public string RegionDescription { get; set; }
     }
 }
